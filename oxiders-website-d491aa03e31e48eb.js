0eaa467a90a270fc5303134f5368ff85bb6558b0
oxide-rs <theluja@gmail.com>
2023-11-16 22:56:15 +0000

    Update from https://github.com/oxide-rs/oxiders-website-dev/commit/6fd31a2685d2cfff81b4e61b3b43a3d498fa55ef

--- oxiders-website-d491aa03e31e48eb.js
+++ oxiders-website-d491aa03e31e48eb.js
@@ -800,7 +800,7 @@ async function __wbg_init(input) {
     if (wasm !== undefined) return wasm;
 
     if (typeof input === 'undefined') {
-        input = new URL('oxiders-website-6ee51901b4702cc_bg.wasm', import.meta.url);
+        input = new URL('oxiders-website-d491aa03e31e48eb_bg.wasm', import.meta.url);
     }
     const imports = __wbg_get_imports();
 
